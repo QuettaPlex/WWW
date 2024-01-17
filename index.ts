78513cb5f0f6d7e8aae6a487d0bda16b6ae3f856
renorari <renorari@renorari.net>
2024-01-17 16:50:43 +0900

    Update express.urlencoded limit to 50mb and add styling to index.html

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 
 app.listen(port, () => {
