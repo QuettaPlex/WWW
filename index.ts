e92e3a94432b13f0ff7a3f349e5ddb1cc5cc94aa
renorari <renorari@renorari.net>
2024-01-17 13:05:09 +0900

    Update server

--- index.ts
+++ index.ts
@@ -5,9 +5,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello, world!");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
