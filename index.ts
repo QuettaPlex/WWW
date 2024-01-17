a61e3d6aa6eaad60fa2d100fd7d65105c91d699f
renorari <renorari@renorari.net>
2024-01-17 13:01:50 +0900

    add web server future

---
+++ index.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
