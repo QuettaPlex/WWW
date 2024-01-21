86f6511e6fe25c30e69bd1ff258b41d701316701
renorari <renorari@renorari.net>
2024-01-21 20:08:20 +0900

    Delete index.js and update dependencies in package.json

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
-const express = require("express");
-require("dotenv").config();
+import cloudflare from "cloudflare";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
