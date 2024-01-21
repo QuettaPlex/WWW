fda90d784d4656ad53ba0d2a50a2e1a3f8a533aa
renorari <renorari@renorari.net>
2024-01-21 22:00:43 +0900

    Add @types/morgan and morgan package dependencies

--- index.ts
+++ index.ts
@@ -1,13 +1,28 @@
-import cloudflare from "cloudflare";
+//import cloudflare from "cloudflare";
 import express from "express";
+import morgan from "morgan";
 import dotenv from "dotenv";
 dotenv.config();
+
+const isDebug = process.env.NODE_ENV === "development";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(isDebug ? morgan("dev") : morgan("combined"));
+app.use((req, res, next) => {
+    if (req.headers["cf-connecting-ip"]) {
+        req.headers["x-forwarded-for"] = req.headers["cf-connecting-ip"];
+    }
+
+    if (!req.headers.host?.includes("quettaplex.com") && !isDebug) {
+        return res.redirect(302, "https://quettaplex.com");
+    }
+
+    next();
+});
 app.use(express.static("public"));
 
 app.listen(port, () => {
