652f1cfc07fa1a8132ffa4bdb8fd6eec85e82fa5
renorari <renorari@renorari.net>
2024-01-21 14:06:27 +0900

    Remove native-node-dns dependency

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
 const express = require("express");
-const dns = require("native-node-dns");
 require("dotenv").config();
 
 const app = express();
-const dnsServer = dns.createServer();
 const port = process.env.PORT || 3000;
-const dnsPort = process.env.DNS_PORT || 53;
-
-dnsServer.on("request", (request, response) => {
-    console.log(request);
-    response.additional.push(dns.A({
-        name: "qhost.rnrr.one",
-        address: "103.81.147.186",
-        ttl: 600
-    }));
-    response.send();
-});
-
-dnsServer.on("error", (err) => {
-    console.log(err.stack);
-});
-
-dnsServer.on("listening", () => {
-    console.log("server listening on", dnsServer.address());
-});
-
-dnsServer.serve(dnsPort);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
