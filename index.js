d07b2996a1c31b414191ea4dfbc2cf494b9d8666
renorari <renorari@renorari.net>
2024-01-21 13:46:44 +0900

    Update DNS server library and add DNS query handling

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
 const express = require("express");
-const dns = require("native-node-dns");
+const named = require("node-named");
 require("dotenv").config();
 
 const app = express();
-const dnsServer = dns.createServer();
+const dnsServer = named.createServer();
 const port = process.env.PORT || 3000;
 const dnsPort = process.env.DNS_PORT || 53;
-
-dnsServer.on("request", (request, response) => {
-    console.log(request);
-    response.additional.push(dns.A({
-        name: "qhost.rnrr.one",
-        address: "103.81.147.186",
-        ttl: 600
-    }));
-    response.send();
-});
-
-dnsServer.on("error", (err) => {
-    console.log(err.stack);
-});
-
-dnsServer.on("listening", () => {
-    console.log("server listening on", dnsServer.address());
-});
-
-dnsServer.serve(dnsPort);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -33,4 +13,16 @@ app.use(express.static("public"));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+dnsServer.listen(dnsPort, "127.0.0.1", function () {
+    console.log("DNS server started on port "+ dnsPort);
+});
+
+dnsServer.on("query", function (query) {
+    var domain = query.name();
+    console.log("DNS Query: %s", domain);
+    var target = named.ARecord("qhost.rnrr.one", "103.81.147.186", { ttl: 600 });
+    query.addAnswer(domain, target, "A");
+    dnsServer.send(query);
 });
