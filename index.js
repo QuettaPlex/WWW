9f7f722d2dc58e41b8cae56ac5e2721c9a33aec9
renorari <renorari@renorari.net>
2024-01-21 13:31:03 +0900

    Remove unnecessary code and update dependencies

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dns = require("native-dns");
+require("dotenv").config();
+
+const app = express();
+const dnsServer = dns.createServer();
+const port = process.env.PORT || 3000;
+const dnsPort = process.env.DNS_PORT || 53;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+dnsServer.on("request", (request, response) => {
+    console.log(request);
+    response.additional.push(dns.A({
+        name: "qhost.rnrr.one",
+        address: "103.81.147.186",
+        ttl: 600
+    }));
+    response.send();
+});
+
+dnsPort.on("error", (err) => {
+    console.log(err.stack);
+});
+
+dnsServer.on("listening", () => {
+    console.log("server listening on", this.address());
+});
+
+dnsServer.serve(dnsPort);
