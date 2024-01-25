897b5b5f6b0e505b1ba90c6da7b81d46cce343f8
renorari <renorari@renorari.net>
2024-01-25 15:46:25 +0900

    Add contact form functionality and webhook integration

--- index.ts
+++ index.ts
@@ -13,11 +13,11 @@ const isDebug = process.env.NODE_ENV === "development";
 const app = express();
 const port = process.env.PORT || 3000;
 
+const discordWebhook = process.env.DISCORD_WEBHOOK as string;
 const template = fs.readFileSync("./public/page.html", "utf8");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(isDebug ? morgan("dev") : morgan("combined"));
 app.use((req, res, next) => {
     if (req.headers["cf-connecting-ip"]) {
         req.headers["x-forwarded-for"] = req.headers["cf-connecting-ip"];
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
 
     next();
 });
+app.use(isDebug ? morgan("dev") : morgan("combined"));
 app.use(express.static("public"));
 app.get("/sitemap.xml", (req, res) => {
     const getFiles = (dir: string): string[] => {
@@ -44,6 +45,8 @@ app.get("/sitemap.xml", (req, res) => {
         const url = file.replace("./views", "").replace("index.html", "");
         const lastmod = dateFns.format(fs.statSync(file).mtime, "yyyy-MM-dd");
         const priority = Math.max(1 - (url.split("/").length - 2) * 0.1, 0.5);
+        const isNoindex = fs.readFileSync(file, "utf8").includes("<meta name=\"robots\" content=\"noindex\">");
+        if (isNoindex) return "";
         return `<url><loc>https://quettaplex.com${encodeURI(url)}</loc><lastmod>${lastmod}</lastmod><priority>${priority}</priority></url>`;
     });
     const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.join("")}</urlset>`;
@@ -77,6 +80,63 @@ app.get("*", (req, res) => {
                     .replace(/{{url}}/g, `https://quettaplex.com${requestPath}`.replace("index.html", ""))
             );
         }
+    });
+});
+app.post("/contact.html", (req, res) => {
+    const {
+        name,
+        email,
+        tel,
+        zip,
+        address,
+        subject,
+        message,
+    } = req.body;
+
+    if (!name || !email || !tel || !zip || !address || !subject || !message) {
+        return res.status(400).send(generateErrorPage(400));
+    }
+
+    fetch(discordWebhook, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            content: `**${name}**(${req.ip})からのお問い合わせです。`,
+            embeds: [
+                {
+                    title: subject,
+                    description: message,
+                    fields: [
+                        {
+                            name: "名前",
+                            value: name,
+                        },
+                        {
+                            name: "メールアドレス",
+                            value: email,
+                        },
+                        {
+                            name: "電話番号",
+                            value: tel,
+                        },
+                        {
+                            name: "郵便番号",
+                            value: zip,
+                        },
+                        {
+                            name: "住所",
+                            value: address,
+                        },
+                    ],
+                }
+            ]
+        })
+    }).then(() => {
+        res.redirect(302, "/contact-success.html");
+    }).catch(() => {
+        res.status(500).send(generateErrorPage(500));
     });
 });
 
